af08ae977c5bc0c7ec59fc1b81858f3fd841c9df
marinaolvera <marinaolvera.it@outlook.com>
2024-07-10 01:57:59 -0600

    MO - Second Commit - Adding Code

---
+++ Programacion/Tetra1/CSharpProjects/Marina_Olvera/Marina_Olvera_Act3_82606.cs
@@ -0,0 +1,35 @@
+﻿namespace Marina_Olvera
+{
+    public class Marina_Olvera_Act3_82606
+    {
+        public void CalculaPrecio()
+        {
+            bool isNumber;
+            do
+            {
+                Console.Write("Ingrese precio : $");
+                string? precioStr = Console.ReadLine();
+                isNumber = int.TryParse(precioStr, out int precio);
+
+                if (!string.IsNullOrEmpty(precioStr) && isNumber)
+                {
+                    double incremento;
+                    if (precio < 1500)
+                    {
+                        incremento = precio * 0.11;
+                    }
+                    else
+                    {
+                        incremento = precio * 0.08;
+                    }
+                    int incrementoEntero = Convert.ToInt32(incremento);
+                    Console.WriteLine($"El incremento es :${incrementoEntero}");
+                    Console.WriteLine($"El nuevo precio es : ${incrementoEntero + precio}");
+                }
+                if (!isNumber)
+                    Console.Clear();
+
+            } while (!isNumber);
+        }
+    }
+}
---
+++ Programacion/Tetra1/CSharpProjects/Marina_Olvera/Program.cs
@@ -0,0 +1,7 @@
+﻿namespace Marina_Olvera
+{
+    public class Program
+    {
+        private static void Main() => new Marina_Olvera_Act3_82606().CalculaPrecio();
+    }
+}
